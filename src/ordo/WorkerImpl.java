e6591aaa609ac8346ebce0ac2f7b1aa516df091d
DoTriN7 <dolouis.trinh@gmail.com>
2020-12-07 23:15:03 +0100

    ajout du main et finition runMap

--- src/ordo/WorkerImpl.java
+++ src/ordo/WorkerImpl.java
@@ -2,7 +2,9 @@ package ordo;
 import map.Mapper;
 import map.Reducer;
 
+import java.net.MalformedURLException;
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
 
 import formats.Format;
 
@@ -18,27 +20,38 @@ public class WorkerImpl implements Worker {
         Format writer;
         CallBack cb;
         
+        public doMap(Mapper m, Format reader, Format writer, CallBack cb) {
+        	this.m = m;
+        	this.reader = reader;
+        	this.writer = writer;
+        	this.cb = cb;
+        }
 		@Override
 		public void run() {
 			this.reader.open(Format.OpenMode.R);
-            this.writer.open(Format.OpenMode.W);
-            this.m.map(this.reader, this.writer);
+			this.writer.open(Format.OpenMode.W);
+			this.m.map(this.reader, this.writer);
             
             //Envoyer le callback qui previent du map fini
+			//TODO!!!!!!
+			
+			this.reader.close();
+            this.writer.close();
             
 		}
     	
     }
     public void runMap(Mapper m, Format reader, Format writer, CallBack cb) throws RemoteException {
         m.map(reader, writer);
-        
-        //map effectué, on ferme reader et writer
-        reader.close();
-        writer.close();
-
-        //Sytem.out.println("Map local terminé");
-
-        //Envoie du callBack cb
-        //cb.???
+       
+        Thread t = new Thread(new doMap(m, reader, writer, cb));
+        // Lancement thread
+        t.start();
+    }
+    
+    public static void main(String args[]) throws RemoteException, MalformedURLException {
+    	int port = 1234;
+    	LocateRegistry.createRegistry(port);
+    	Naming.rebind("ADRESSE A DEFINIR!!!!", new WorkerImpl());
     }
 }
