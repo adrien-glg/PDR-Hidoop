7d81aa1d283fd0d7533993fdd4d71d1faeead345
ThibaudMrx <75337223+ThibaudMrx@users.noreply.github.com>
2020-12-10 22:14:22 +0100

    Update HdfsServer.java

--- src/hdfs/HdfsServer.java
+++ src/hdfs/HdfsServer.java
@@ -21,7 +21,7 @@ import java.net.Socket;
 public class HdfsServer extends Thread {
 	private Socket socket;
 	Format file;
-	Format.Type fmt;
+	Format.Type type_fichier;
 	KV kv;
 	static String PATH = Project.PATH;
 	static String DATA = Project.DATAN7;
@@ -48,94 +48,92 @@ public class HdfsServer extends Thread {
 			System.out.println("Connexion réussie");
 			// Transfert de donnees du client au serveur
 			final ObjectInputStream objectInputStream = new ObjectInputStream(this.socket.getInputStream());
-			// Transfert de donn�es du serveur au client
+			// Transfert de données du serveur au client
 			// final OutputStream outputOS = socket.getOutputStream();
-			// On re�oit la commande � ex�cuter plus le nom de fragment
-			final String[] split = ((String) objectInputStream.readObject()).split(" ");
-			// Nom de fragment
-			final String nomFichier = split[1];
+			// On reçoit la commande à exécuter plus le nom de fragment
+			final String[] split_commande = ((String) objectInputStream.readObject()).split(" ");
+			
+			// Une commande est de la forme CMD_WRITE nom_fichier&LINE&texte_du_fragment
+			// On fait alors deux split successifs : l'un sur le type de la commande, l'autre sur les données fournies
+			final String typeCommande = split_commande[0];
+			final String fichier = split_commande[1];
+
 			Commande commande;
 
-			switch (split[0]) {
+			switch (typeCommande) {
 			case "CMD_WRITE":
 				commande = Commande.CMD_WRITE;
-				break;
-			case "CMD_READ":
-				commande = Commande.CMD_READ;
-				break;
-			default:
-				commande = Commande.CMD_DELETE;
-			}
-
-			
-			
-			if (commande == Commande.CMD_WRITE) {
-
 				// On ouvre le fichier cree en mode ecriture afin d'y ecrire les parties du
 				// fragment
 				// On recoit le format du fichier du fragment à recevoir
-				fmt = (Format.Type) objectInputStream.readObject();
+				
+				// Second split : nom_fichier&KV&clé1<->valeur1\ncle2<->valeur2\ncle3<->valeur3
+				// On commence par séparer le nom du fichier, de son type, et des donnees
+				final String[] splitFichier = fichier.split("$");
+				final String nom_fichier = splitFichier[0];
+				final String fmt_str = splitFichier[1];
+				final Format.Type fmt = fmt_str == "LINE" ? Format.Type.LINE : Format.Type.KV;
+				final String donnees = splitFichier[2];
+				
+				// Troisieme split : clé1<->valeur1\ncle2<->valeur2\ncle3<->valeur3
+				// On sépare les KV entre eux avec les retours chariot, puis on reconstruit chaque KV
+				final String[] tab_KV = donnees.split("\n");
+				
+				// On ouvre le fichier pour le préparer au travail
 				if (fmt == Format.Type.KV)
-					this.file = (Format) new KVFormat(PATH + DATA + nomFichier);
+					this.file = (Format) new KVFormat(PATH + DATA + fichier);
 				else
-					this.file = new LineFormat(PATH + DATA + nomFichier);
+					this.file = new LineFormat(PATH + DATA + fichier);
 				this.file.open(Format.OpenMode.W);
-				// On recoit le premier element du fragment sous forme de KV
-				final String s2 = (String) objectInputStream.readObject();
-				boolean estNul = false;
-				KV kv = null;
-				if (s2 == null) {
-					estNul = true;
-				} else {
-					final String[] split2 = s2.split("<->");
-					// On recree le KV recu
-					kv = new KV(split2[0], split2[1]);
-				}
-				while (!estNul) {
-					// On ecrit le KV recu dans le fichier
+				
+				for (String kv_str : tab_KV) {
+					// On split les deux valeurs du KV : cle<->valeur
+					String[] kv_split = kv_str.split("<->");
+					// On recrée le KV reçu
+					kv = new KV(kv_split[0], kv_split[1]);
+					// On l'ajoute au fichier
 					this.file.write(kv);
-					// On recoit toutes les parties du fragment
-
-					try {
-						final Object object = objectInputStream.readObject();
-						final String[] split3 = ((String) object).split("<->");
-						kv = new KV(split3[0], split3[1]);
-					} catch (Exception e) {
-						estNul = true;
-					}
 				}
-
+					
 				// BufferedWriter buff = new BufferedWriter(new OutputStreamWriter(outputOS));
-				System.out.println(nomFichier);
+				System.out.println(fichier);
 				// outputOS.write(1);
 				this.file.close();
 				objectInputStream.close();
 				this.socket.close();
-
-			} else if (commande == Commande.CMD_READ) {
+				break;
+				
+				
+			case "CMD_READ":
+				commande = Commande.CMD_READ;
+				// Dans ce cas, une commande est CMD_READ nomFichier
+				// Le split de départ suffit alors, avec fichier = nomFichier
 				// Transfert de donnees du serveur au client (read)
 				final ObjectOutputStream objectOutputStream = new ObjectOutputStream(this.socket.getOutputStream());
-				Format file = new KVFormat(PATH + DATA + nomFichier);
+				Format file = new KVFormat(PATH + DATA + fichier);
 				file.open(Format.OpenMode.R);
-				KV kv;
-
-				while ((kv = file.read()) != null) {
-					String objetKV = kv.k + "<->" + kv.v;
-					System.out.println(objetKV);
-					objectOutputStream.writeObject(objetKV);
+				KV kv_a_envoyer;
+				// On renvoie chaque kv au client
+				while ((kv_a_envoyer = file.read()) != null) {
+					String kv_str = kv_a_envoyer.k + "<->" + kv_a_envoyer.v;
+					System.out.println(kv_str);
+					objectOutputStream.writeObject(kv_str);
 				}
 				objectOutputStream.writeObject(null);
 				System.out.println("Fin de connexion");
 				file.close();
 				objectInputStream.close();
 				objectOutputStream.close();
-
-			} else {
+				break;
+				
+				
+				
+			default:
 				// Transfert de donnees du serveur au client (accuse de reception pour client de la suppression)
-				final ObjectOutputStream objectOutputStream = new ObjectOutputStream(this.socket.getOutputStream());
-				objectOutputStream.writeObject(new File(PATH+DATA+nomFichier).delete());
+				commande = Commande.CMD_DELETE;
+				final ObjectOutputStream objectOutputStream1 = new ObjectOutputStream(this.socket.getOutputStream());
+				objectOutputStream1.writeObject(new File(PATH+DATA+fichier).delete());
 				objectInputStream.close();
-
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
