6fbb737ed529d901e40a23965795d64771efadf0
Lucas Oros <ioroscampo@gmail.com>
2020-12-10 13:40:04 +0100

    Constructeur hdfs client

--- src/hdfs/HdfsClient.java
+++ src/hdfs/HdfsClient.java
@@ -5,8 +5,23 @@ import formats.Format;
 import formats.KV;
 import formats.KVFormat;
 import formats.LineFormat;
+import configuration.Parametres; 
 
 public class HdfsClient {
+
+    private Socket[] sockets;
+
+    /*Constructeur qui permet de définir une liste de sockets en fonction du fichier configuration défini*/ 
+    public HdfsClient(){
+        /*Nombre de sockets à définir*/ 
+        int n = Parametres.adresses.length;
+        sockets = new Socket[n]; 
+        /*Boucle initialisant chaque socket et réalisant une démande de connexion*/
+        for (int i = 0; i<n; i++){
+            sockets[i] = new Socket(Parametres.adresses[i],Parametres.ports[i]);
+            println("Connecté à la machine" + Parametres.adresses[i] + " sur le port " + Parametres.ports[i]);
+        }
+    }
 
     private static void usage() {
         System.out.println("Usage: java HdfsClient read <file>");
@@ -19,17 +34,27 @@ public class HdfsClient {
     public static void HdfsWrite(Format.Type fmt, String localFSSourceFname, 
      int repFactor) { }
 
-    public static void HdfsRead(String hdfsFname, String localFSDestFname) { }
+    public static void HdfsRead(String hdfsFname, String localFSDestFname) {
+        /*Déclaraiton des OutputStream qui serviront pour envoyer la commande aux nodes*/ 
+        
+        /*Definition du message qui va être envoyé par le socket*/
+
+        /**/  
+
+     }
 
 	
     public static void main(String[] args) {
         // java HdfsClient <read|write> <line|kv> <file>
 
+        
         try {
             if (args.length<2) {usage(); return;}
+            /*Initialisation des sockets selon le fichier configuration*/
+            this = new HdfsClient(); 
 
             switch (args[0]) {
-              case "read": HdfsRead(args[1],null); break;
+              case "read": HdfsRead(args[1],Parametres.path + args[1]); break;
               case "delete": HdfsDelete(args[1]); break;
               case "write": 
                 Format.Type fmt;
